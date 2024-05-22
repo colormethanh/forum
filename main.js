eff6036149cb205d1b881d065740137fefc00121
Thanh Nguyen <thnguyen1847@gmail.com>
2024-05-21 22:55:06 -0400

    added like, dislike, sort, and delete function

--- main.js
+++ main.js
@@ -4,7 +4,62 @@ import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 var form = document.getElementById("form");
 var postsContainer = document.getElementById("posts-container");
 
-var createPostContentRowElement = function(message, author, id){
+var handleLike = function (id){
+  var postsData = JSON.parse(localStorage.getItem("PARSITYFORUM"));
+  var postsArray = postsData.posts;
+
+  let updatedPostsArray = postsArray.map((post) => {
+    if (post.id === id) {
+      post.upVotes += 1;
+      return post
+    }
+    return post;
+  });
+
+  postsData.posts = updatedPostsArray;
+  localStorage.setItem("PARSITYFORUM", JSON.stringify(postsData));
+
+  updatePostsContainer();
+  return 1;
+}
+
+var handleDislike = function(id) {
+  var postsData = JSON.parse(localStorage.getItem("PARSITYFORUM"));
+  var postsArray = postsData.posts;
+
+  let updatedPostsArray = postsArray.map((post) => {
+    if (post.id === id) {
+      post.downVotes += 1;
+      return post
+    }
+    return post;
+  });
+
+  postsData.posts = updatedPostsArray;
+  localStorage.setItem("PARSITYFORUM", JSON.stringify(postsData));
+
+  updatePostsContainer();
+  return 1;
+}
+
+var handleDeletePost = function(id) {
+  var postsData = JSON.parse(localStorage.getItem("PARSITYFORUM"));
+  var postsArray = postsData.posts;
+
+  let updatedPostsArray = postsArray.reduce((newArray, post) => {
+    if (post.id !== id) newArray.push(post);
+    return newArray
+  }, [])
+
+  postsData.posts = updatedPostsArray;
+  localStorage.setItem("PARSITYFORUM", JSON.stringify(postsData));
+
+  updatePostsContainer();
+  return 1;
+
+};
+
+var createPostContentRowElement = function(message, author){
   var postContentRow = document.createElement("div")
   postContentRow.setAttribute("class", "row post-content");
 
@@ -21,13 +76,14 @@ var createPostContentRowElement = function(message, author, id){
   return postContentRow;
 }
 
-var createPostOptionsRowElement = function (upVotes, downVotes) {
+var createPostOptionsRowElement = function (upVotes, downVotes, id) {
   var postOptionsRow = document.createElement("div")
   postOptionsRow.setAttribute("class", "row post-options justify-content-between");
   
   var likeBtnColumn = document.createElement("div")
   likeBtnColumn.setAttribute("class", "col-sm-8");
   
+  // Like Btn
   var likeBtn = document.createElement("button");
   likeBtn.setAttribute("class", "btn btn-success like-btn me-2");
   var likeBtnIcon = document.createElement("i");
@@ -36,6 +92,12 @@ var createPostOptionsRowElement = function (upVotes, downVotes) {
   likeBtn.innerHTML += " " + upVotes;
   likeBtnColumn.appendChild(likeBtn);
 
+  likeBtn.addEventListener("click", (event) => {
+    handleLike(id);
+  });
+
+
+  // Dislike btn
   var dislikeBtn = document.createElement("button");
   dislikeBtn.setAttribute("class", "btn btn-danger dislike-btn");
   var dislikeBtnIcon = document.createElement("i");
@@ -43,6 +105,10 @@ var createPostOptionsRowElement = function (upVotes, downVotes) {
   dislikeBtn.appendChild(dislikeBtnIcon);
   dislikeBtn.innerHTML += " " + downVotes;
   likeBtnColumn.appendChild(dislikeBtn);
+
+  dislikeBtn.addEventListener("click", (event) => {
+    handleDislike(id);
+  });
   
 
   var deleteBtnColumn = document.createElement("div")
@@ -51,6 +117,10 @@ var createPostOptionsRowElement = function (upVotes, downVotes) {
   deleteBtn.innerHTML = "Delete Post"
   deleteBtn.setAttribute("class", "delete-btn");
   deleteBtnColumn.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", (event) => {
+    handleDeletePost(id);
+  });
 
   postOptionsRow.appendChild(likeBtnColumn);
   postOptionsRow.appendChild(deleteBtnColumn);
@@ -66,8 +136,8 @@ var createPostElement = function(post){
   var postElementInner = document.createElement("div")
   postElementInner.setAttribute("class", "col-12 post-element-inner");
   
-  var postContentRow = createPostContentRowElement(post.message, post.author, post.id);
-  var postOptionsRow = createPostOptionsRowElement(post.upVotes, post.downVotes);
+  var postContentRow = createPostContentRowElement(post.message, post.author);
+  var postOptionsRow = createPostOptionsRowElement(post.upVotes, post.downVotes, post.id);
 
   // Post divider
   var postDivider = document.createElement("hr");
@@ -86,7 +156,6 @@ var createPostElement = function(post){
 
 var addPostToLocalStorage = function (message, author) {
   var postsData = JSON.parse(localStorage.getItem("PARSITYFORUM"))
-  debugger
   if (!postsData) {
     localStorage.setItem("PARSITYFORUM", JSON.stringify({"posts": []}))
     var postsData = JSON.parse(localStorage.getItem("PARSITYFORUM"))
@@ -99,16 +168,36 @@ var addPostToLocalStorage = function (message, author) {
   return newPost
 }
 
+var sortPosts = function(post1, post2){
+  // debugger
+  var post1Score = post1.upVotes - post1.downVotes;
+  var post2Score = post2.upVotes - post2.downVotes;
+
+  if (post1Score > post2Score){
+    return -1;
+  } else if (post1Score === post2Score) {
+    return 0;
+  }
+  return 1;
+}
+
 var clearPosts = function(){
-  var postElements = document.getElementsByClassName("post-element");
-  console.log(postElements)
+  var postElements = Array.from(document.getElementsByClassName("post-element"));
+
+  if (postElements.length === 0) return;
+
+  postElements.forEach((element) => {
+    element.remove();
+  })
 }
 
 var populatePosts = function(){
   var postsData = JSON.parse(localStorage.getItem("PARSITYFORUM"))
-  if (!postsData) return;
+  if (!postsData || postsData.posts.length === 0) return;
 
   var postsArray = postsData.posts;
+
+  postsArray = postsArray.sort(sortPosts);
 
   var postElements = postsArray.map(post => {
     return createPostElement(post)
@@ -117,7 +206,11 @@ var populatePosts = function(){
   postElements.forEach(post => {
     postsContainer.appendChild(post);
   });
+}
 
+var updatePostsContainer = function(){
+  clearPosts();
+  populatePosts();
 }
 
 
@@ -132,10 +225,9 @@ form.addEventListener("submit", (e) => {
   
   let newPost = addPostToLocalStorage(message.value, user.value);
   let newPostElement = createPostElement(newPost);
-  console.log(newPost)
-  postsContainer.appendChild(newPostElement)
-  user.value = ""
-  message.value = ""
+  updatePostsContainer();
+  user.value = "";
+  message.value = "";
 })
 
 
